a7f7bf9d3bdbbf793fc25f48df18d681db532f85
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-05-20 13:03:35 +0800

    ✅ test: add testing case

--- packages/utils/test/object.test.ts
+++ packages/utils/test/object.test.ts
@@ -1,10 +1,10 @@
-import { describe,expect, it } from "vitest"
+import { describe, expect, it } from "vitest"
 
-import { entries,Extends,exclude } from "@/object"
+import { entries, Extends, exclude, renameKey } from "@/object"
 
-describe("object testing",()=>{
-   it("test entries",()=>{
-      let obj:any[] = [12,45,"hello"]
+describe("object testing", () => {
+   it("test entries", () => {
+      let obj: any[] = [12, 45, "hello"]
       expect(entries(obj)).toMatchInlineSnapshot(`
         [
           [
@@ -21,10 +21,10 @@ describe("object testing",()=>{
           ],
         ]
       `)
-      let map = new Map<string,any>();
-      map.set("a",1)
-      map.set("b",34)
-      map.set("name",36)
+      let map = new Map<string, any>();
+      map.set("a", 1)
+      map.set("b", 34)
+      map.set("name", 36)
 
       expect(entries(map)).toMatchInlineSnapshot(`
         [
@@ -42,12 +42,65 @@ describe("object testing",()=>{
           ],
         ]
       `)
-      it("test exclude",()=>{
+      expect(entries([12, 33, 56, 89])).toMatchInlineSnapshot(`
+        [
+          [
+            0,
+            12,
+          ],
+          [
+            1,
+            33,
+          ],
+          [
+            2,
+            56,
+          ],
+          [
+            3,
+            89,
+          ],
+        ]
+      `)
+      expect(entries({
+         name:"Typescript",
+         type:"Programing Language"
+      })).toMatchInlineSnapshot(`
+        [
+          [
+            "name",
+            "Typescript",
+          ],
+          [
+            "type",
+            "Programing Language",
+          ],
+        ]
+      `)
+   })
+   it("test exclude", () => {
       expect(exclude({
-         name:"balabala",
-         age:33
-      },["age"])).equal({name:"balabala"})
-    })
+         name: "balabala",
+         age: 33
+      }, ["age"])).toMatchInlineSnapshot(`
+        {
+          "name": "balabala",
+        }
+      `)
+   })
+   it("testing rename key", () => {
+      expect(renameKey({
+         "a": "Bib",
+         "b": "1h"
+      }, {
+         a: "name",
+         b: "time"
+      })).toMatchInlineSnapshot(`
+        {
+          "name": "Bib",
+          "time": "1h",
+        }
+      `)
    })
 
 })
