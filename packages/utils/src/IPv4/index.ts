bce583661e53778df5a8440ca85c5274109aba7e
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-06-04 14:49:42 +0800

    ✨ feat: add class `IPv4` ✅ test: add IPv4 testing case

---
+++ packages/utils/src/IPv4/index.ts
@@ -0,0 +1,71 @@
+import { isNumber, isString } from "@/typeis"
+
+export class IPv4 {
+   ipv4: string = ""
+   integer: number = 0
+
+   constructor(ip: string | number) {
+      if(isString(ip)){
+         this.ipv4 = ip
+      }else if(isNumber(ip)){
+         this.integer = ip
+      }
+   }
+   /**
+    * [_toString description]
+    * @param {number} ip [description]
+    */
+   _toString(ip: number) {
+      return `${(ip >> 24) & 0xff}.${(ip >> 16) & 0xff}.${(ip >> 8) & 0xff}.${(ip >> 0) & 0xff}`
+   }
+   /**
+    * [_toNumber description]
+    * @param {string = ""} address [description]
+    */
+   _toNumber(address: string = "") {
+      if (address != "") {
+         let ipNumber = 0
+         let partIp = address.split(".")
+         let i = 0
+         for (let ipart of partIp) {
+            ipNumber += parseInt(ipart) * (0x100 ** (3 - i))
+            i++;
+         }
+         return ipNumber
+      }
+   }
+   toString() {
+      return this._toString(this.integer)
+   }
+   toNumber() {
+      return this._toNumber(this.ipv4)
+   }
+   /**
+    * @param {string} start start condition of the iterator
+    * @param {string} end start condition of the iterator
+    *
+    * @example
+    * ```js
+    * for(let ip of generator("127.0.0.1","192.168.255.255")){
+    *    console.log(ip)
+    * }
+    * ```
+    */
+   *generator(start: string = "", end: string = "") {
+      if (start != "" && end != "") {
+         let next = this._toNumber(start) as number
+         let final = this._toNumber(end) as number
+         if (start < end) {
+            while (next < final) {
+               yield this._toString(next)
+               next++
+            }
+         }
+      }
+   }
+}
+
+export function useIPv4(ip:string|number){
+   let ip_ = new IPv4(ip)
+   return ip_
+}
---
+++ packages/utils/test/ipv4.test.ts
@@ -0,0 +1,13 @@
+import { useIPv4 } from "@/IPv4"
+import { describe, expect, it } from "vitest"
+
+describe("testing ipv4 case",()=>{
+   it("testing ipv4 toNumber",()=>{
+      let ip = useIPv4("127.0.0.1")
+      expect(ip.toNumber()).eq(2130706433)
+   })
+   it("testing ipv4 toString",()=>{
+      let ip = useIPv4(2130706433)
+      expect(ip.toString()).eq("127.0.0.1")
+   })
+})
