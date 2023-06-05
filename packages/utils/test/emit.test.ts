4dc0a49f0e9b3daf22840321dcac332959f7613c
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-06-05 14:33:02 +0800

    🎨format: format emit test case

--- packages/utils/test/emit.test.ts
+++ packages/utils/test/emit.test.ts
@@ -1,17 +1,17 @@
-import { describe,expect, it } from "vitest"
-import { useEmit } from "@/emit"
-describe("test emit",()=>{
-    let { Emit,Handler } = useEmit()
-    it("test emit handler",()=>{
-        class User{
-            @Emit("user")
-            name(...args:any[]){
-                console.log("hello")
-                return args
-            }
-        }
-        let { user } = Handler("user",1,2,3,4,5,6)
-        expect(user).toMatchInlineSnapshot(`
+import { describe, expect, it } from 'vitest';
+import { useEmit } from '@/emit';
+describe('test emit', () => {
+   let { Emit, Handler } = useEmit();
+   it('test emit handler', () => {
+      class User {
+         @Emit('user')
+         name(...args: any[]) {
+            console.log('hello');
+            return args;
+         }
+      }
+      let { user } = Handler('user', 1, 2, 3, 4, 5, 6);
+      expect(user).toMatchInlineSnapshot(`
           [
             [
               [
@@ -25,5 +25,5 @@ describe("test emit",()=>{
             ],
           ]
         `);
-    })
-})
+   });
+});
