0221b63d5989b82c785a154f56b24999f44ab0d6
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-06-05 14:32:15 +0800

    🎨format: format documentCache

--- packages/utils/src/documentCache/index.ts
+++ packages/utils/src/documentCache/index.ts
@@ -1,69 +1,67 @@
-import { isUndefined } from "@/typeis";
-import { duration } from "../duration";
+import { isUndefined } from '@/typeis';
+import { duration } from '../duration';
 type ExpiresValue<T> = {
    [P in keyof T]: {
-      value: T[P],
-      expires: number
-   }
-}
+      value: T[P];
+      expires: number;
+   };
+};
 export class DocumentCache<T extends object & {}> {
-   private callbacks: Utils.Dict<"change" | "changeTime" | "changeValue" | "timeout", (() => void)[]> = {
+   private callbacks: Utils.Dict<'change' | 'changeTime' | 'changeValue' | 'timeout', (() => void)[]> = {
       change: [],
       changeTime: [],
       changeValue: [],
-      timeout: []
+      timeout: [],
    };
    $data: ExpiresValue<T>;
    constructor(data: object = {}) {
-      let self = this
+      let self = this;
       this.$data = {} as ExpiresValue<T>;
-      this.callbacks
+      this.callbacks;
       if (isUndefined(Proxy)) {
-         Object.keys(data).forEach((key) => {
+         Object.keys(data).forEach(key => {
             this.$data[key as keyof T] = {
                value: (data as any)[key],
-               expires: duration(new Date(), "1m").getTime()
-            }
+               expires: duration(new Date(), '1m').getTime(),
+            };
             Object.defineProperty(this, key, {
                get() {
-                  let now = Date.now()
+                  let now = Date.now();
                   if (now < self.$data[key as keyof T].expires) {
-                     return self.$data[key as keyof T].value
+                     return self.$data[key as keyof T].value;
                   }
                },
                set(v) {
                   self.$data[key as keyof T].value = v;
                },
-            })
-         })
+            });
+         });
       } else {
          new Proxy(self, {
             get(target: DocumentCache<{}>, p: string) {
-               let val = Reflect.get(self.$data, p) as {value: any,expires: number};
-               if(Date.now() < val.expires){
-                  return val.value
+               let val = Reflect.get(self.$data, p) as { value: any; expires: number };
+               if (Date.now() < val.expires) {
+                  return val.value;
                }
                return;
             },
-            set(target:DocumentCache<{}>, p:string, newValue:any): boolean {
-               let val = Reflect.get(self,p) as {value: any,expires: number};
-               if(Date.now() < val.expires){
-                  return Reflect.set(self.$data,p,newValue)
+            set(target: DocumentCache<{}>, p: string, newValue: any): boolean {
+               let val = Reflect.get(self, p) as { value: any; expires: number };
+               if (Date.now() < val.expires) {
+                  return Reflect.set(self.$data, p, newValue);
                }
-               return false
-            }
-         })
+               return false;
+            },
+         });
       }
    }
-   setTime(key: keyof T, time: `${number}${Utils.DurationUnits}` = "10m") {
-
+   setTime(key: keyof T, time: `${number}${Utils.DurationUnits}` = '10m') {
       this.$data[key].expires = duration(new Date(), time).getTime();
    }
-   on(eventName: "change" | "changeTime" | "changeValue" | "timeout", callback: () => void) {
+   on(eventName: 'change' | 'changeTime' | 'changeValue' | 'timeout', callback: () => void) {
       // TODO:
-      this.callbacks[eventName].push(callback)
+      this.callbacks[eventName].push(callback);
    }
-
 }
 
 function useCeche<T extends {}>(obj: T) {
@@ -71,5 +69,5 @@ function useCeche<T extends {}>(obj: T) {
    function getCache() {
       return caches;
    }
-   return getCache()
+   return getCache();
 }
