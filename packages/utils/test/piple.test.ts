0823032184e0ac68cff67a7a7f1191b14f82e16f
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-06-18 12:16:22 +0800

    ✅ test: add pipel testing case

--- packages/utils/test/piple.test.ts
+++ packages/utils/test/piple.test.ts
@@ -1,18 +1,31 @@
 import { usePipel } from '@/pipel';
 import { expect, it, describe } from 'vitest';
 
-describe('testing piple', function () {
-   let [add,run] = usePipel()
-   it('testing piple 1', () => {
+describe('testing pipel', function () {
+   it('testing pipel 1', () => {
+      let [add, run] = usePipel();
       let double = (a: number) => {
          return a * 2;
       };
       let mult = (a: number) => {
          return a - 3;
       };
-      add(double,mult)
-      run(10).then((val)=>{
-         expect(val).eq(17)
-      })
+      add(double, mult);
+      run(10).then(val => {
+         expect(val).eq(17);
+      });
+   });
+   it('testing pipel 2', () => {
+      let [add, run, handler] = usePipel();
+      let our = () => {
+         throw Error('testing exepection');
+      };
+      handler((e, result) => {
+         if (e) {
+            console.log(e);
+         }
+      });
+      add(our);
+      run(1);
    });
 });
