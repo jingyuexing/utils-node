0fa598f0567d3bdc12f977d20a60b8c6b4902d99
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-07-05 03:43:13 +0800

    ♻️ refactor: optimizing judgment conditions

--- packages/utils/src/lazyLoading/index.ts
+++ packages/utils/src/lazyLoading/index.ts
@@ -1,38 +1,42 @@
 export function lazyLoading<T extends Element>(eles: T[] | NodeListOf<T>, callback: (entry: T) => void) {
    let elementList = [...eles];
-   if (typeof IntersectionObserver !== "undefined") {
-      let observer = new IntersectionObserver((entries) => {
-         entries.forEach((entry) => {
+   if (typeof IntersectionObserver !== 'undefined') {
+      let observer = new IntersectionObserver(entries => {
+         entries.forEach(entry => {
             if (entry.isIntersecting) {
-               callback(entry.target as T)
-               observer.unobserve(entry.target)
+               callback(entry.target as T);
+               observer.unobserve(entry.target);
             }
-         })
-      })
-      eles.forEach((value) => {
+         });
+      });
+      eles.forEach(value => {
          observer.observe(value);
-      })
+      });
    } else {
       let isVisiable = (ele: Element) => {
-
          let elBounding = ele.getBoundingClientRect();
-         let windowsHeight = window.innerHeight || document.documentElement.clientHeight
+         let windowsHeight = window.innerHeight || document.documentElement.clientHeight;
          let windowsWidth = window.innerWidth || document.documentElement.clientWidth;
-         return elBounding.top >= 0 && elBounding.left >= 0 && elBounding.bottom <= windowsHeight && elBounding.right <= windowsWidth;
-      }
+         return (
+            ((elBounding.top > 0 && elBounding.top <= windowsHeight) ||
+               (elBounding.bottom > 0 && elBounding.bottom <= windowsHeight)) &&
+            ((elBounding.right > 0 && elBounding.right <= windowsWidth) ||
+               (elBounding.left > 0 && elBounding.left <= windowsWidth))
+         );
+      };
       let func = (e: Event) => {
-         elementList.forEach((value) => {
+         elementList.forEach(value => {
             if (isVisiable(value)) {
-               callback(value)
+               callback(value);
                elementList.findIndex((el, idx) => {
                   if (el == value) {
                      delete elementList[idx];
                   }
-               })
+               });
             }
-         })
-      }
-      document.addEventListener("scroll", func)
-      document.addEventListener("resize", func)
+         });
+      };
+      document.addEventListener('scroll', func);
+      document.addEventListener('resize', func);
    }
 }
