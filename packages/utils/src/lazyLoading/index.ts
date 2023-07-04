1ef78bc6c015989570944839b95ce8702365a882
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-07-05 03:22:53 +0800

    ♻️ refactor:improve image loading way

--- packages/utils/src/lazyLoading/index.ts
+++ packages/utils/src/lazyLoading/index.ts
@@ -1,4 +1,5 @@
-export function lazyLoading<T extends Element>(ele: T, callback: (entry:T) => void) {
+export function lazyLoading<T extends Element>(eles: T[] | NodeListOf<T>, callback: (entry: T) => void) {
+   let elementList = [...eles];
    if (typeof IntersectionObserver !== "undefined") {
       let observer = new IntersectionObserver((entries) => {
          entries.forEach((entry) => {
@@ -8,18 +9,28 @@ export function lazyLoading<T extends Element>(ele: T, callback: (entry:T) => vo
             }
          })
       })
-      observer.observe(ele);
+      eles.forEach((value) => {
+         observer.observe(value);
+      })
    } else {
-      let isVisiable = () => {
+      let isVisiable = (ele: Element) => {
+
          let elBounding = ele.getBoundingClientRect();
          let windowsHeight = window.innerHeight || document.documentElement.clientHeight
          let windowsWidth = window.innerWidth || document.documentElement.clientWidth;
          return elBounding.top >= 0 && elBounding.left >= 0 && elBounding.bottom <= windowsHeight && elBounding.right <= windowsWidth;
       }
       let func = (e: Event) => {
-         if (isVisiable()) {
-            callback(ele);
-         }
+         elementList.forEach((value) => {
+            if (isVisiable(value)) {
+               callback(value)
+               elementList.findIndex((el, idx) => {
+                  if (el == value) {
+                     delete elementList[idx];
+                  }
+               })
+            }
+         })
       }
       document.addEventListener("scroll", func)
       document.addEventListener("resize", func)
