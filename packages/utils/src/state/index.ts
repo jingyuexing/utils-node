cfce341f1b02fc9527c036914f40b91e370a7d75
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-07-31 15:01:23 +0800

    🧹 chore: improve typing

--- packages/utils/src/state/index.ts
+++ packages/utils/src/state/index.ts
@@ -1,13 +1,12 @@
 type StateLoop = "observe" | "action" | "decision" | "orientation";
 
 type ActionMap<State extends string[]> = {
-  [K in State[number]]:(state: K) => ReturnType<ActionMap<State>[K]>;
+  [K in `on${State[number]}`]:(state: State[number]) => ReturnType<ActionMap<State>[K]>;
 };
 export function useStateMachine<State extends string[]>(state: State, action: ActionMap<State>) {
   const _state: State = state;
-  let _status: State[number] = _state[0];
-
-  let _currentAction: ActionMap<State>[State[number]] = action[_status];
+  let _status: `on${State[number]}` = `on${_state[0]}`;
+  let _currentAction: ActionMap<State>[`on${State[number]}`] = action[_status];
   let _change: (before:State[number],current: State[number]) => void = () => {};
   let _crash:(e:Error,state:State[number])=>void;
   const setState = (state: State[number]) => {
@@ -25,7 +24,7 @@ export function useStateMachine<State extends string[]>(state: State, action: Ac
          }
       }
     }
-    _status = state;
+    _status = `on${state}`;
     _currentAction = action[_status];
   };
   const invoke = ()=>{
