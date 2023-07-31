75b6c68967bdbc95dfc9bd8bc0b313bdf8422e9e
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-07-31 14:31:16 +0800

    ♻️ refactor: fix full logic

--- packages/utils/src/DOM/fullscreen.ts
+++ packages/utils/src/DOM/fullscreen.ts
@@ -7,16 +7,21 @@ export function useFullScreen(): FullScreenReturns {
     let _switch = false
     let _targetDOM: Element = document.body;
     let _errorHandler: (e: Error) => void
-    const intoFull = (options?: FullscreenOptions) => {
+    const full = (options?: FullscreenOptions) => {
         if (_switch && !document.fullscreenElement) {
             _targetDOM.requestFullscreen(options).catch((reason) => {
                 _errorHandler(reason)
             })
+        }else if(!_switch && document.fullscreenElement){
+            document.exitFullscreen().catch((reason)=>{
+                _errorHandler(reason)
+            })
         }
     }
+    
     const toggle = (options?: FullscreenOptions) => {
         _switch = !_switch
-        intoFull(options)
+        full(options)
     }
     const error = (cb: (e: Error) => void) => {
         _errorHandler = cb
