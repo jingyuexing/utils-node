87dab01ac4aa18b8048c1b529b57c0e70244e756
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-08-03 22:49:56 +0800

    🎨format: format code

--- packages/utils/src/CSV/index.ts
+++ packages/utils/src/CSV/index.ts
@@ -1,8 +1,8 @@
 type Delimiter = ',' | ';' | '，' | '；' | '|' | ' ' | '\t' | '$' | '#' | '.' | '~' | '-' | '_';
 function guessDelimiter(line: string): Delimiter | null {
    const possibleDelimiters: Delimiter[] = [',', ';', '，', '；', '|', ' ', '\t', '$', '#', '.', '~', '-', '_'];
-   var delimiter: Delimiter | null = null;
-   for (var i = 0; i < possibleDelimiters.length; i++) {
+   let delimiter: Delimiter | null = null;
+   for (let i = 0; i < possibleDelimiters.length; i++) {
       const parts = line.split(possibleDelimiters[i]);
       if (parts.length > 1) {
          delimiter = possibleDelimiters[i];
