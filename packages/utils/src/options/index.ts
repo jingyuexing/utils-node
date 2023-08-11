57f1133df6686d3bf52592b1d2c50a9d5fc4e612
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-08-11 22:57:51 +0800

    🧹 chore: move to options folder

---
+++ packages/utils/src/options/index.ts
@@ -0,0 +1,62 @@
+import { isNone } from '../typeis'
+import type { Nullable,Options, OptionsInspectCallback,Some as Some_,None as None_ } from '../types'
+export function None<T>(): None_<T> {
+  return {
+    isSome: () => false,
+    isNone: () => true,
+    unwrap: () => 'None',
+    unwrapOr: <Default>(defaultValue: Default) => defaultValue,
+    isSomeAnd: () => false,
+    expect: (msg: string) => {
+      throw new Error(msg);
+    },
+    inspect: () => None(),
+  };
+}
+
+export function Some<T>(value: T): Some_<T> {
+  return {
+    isSome: () => true,
+    isNone: () => false,
+    unwrap: () => {
+      if (isNone(value)) {
+        console.error('Value is None');
+        return 'None';
+      }
+      return value;
+    },
+    unwrapOr: <Default>(defaultValue: Default) => {
+      if (isNone(value)) {
+        return defaultValue;
+      }
+      return value;
+    },
+    isSomeAnd: (callback:(val:T)=>boolean) => {
+      if (isNone(value)) {
+        return false;
+      }
+      return callback(value);
+    },
+    expect: (msg: string) => {
+      if (isNone(value)) {
+        throw new Error(msg);
+      }
+      return value;
+    },
+    inspect: (callback: OptionsInspectCallback<T>) => {
+      if (isNone(value)) {
+        return None();
+      }
+      callback(value);
+      return Some(value);
+    },
+  };
+}
+
+export function Option<T>(value: Nullable<T>): Options<T> {
+  if (isNone(value)) {
+    return None();
+  } else {
+    return Some(value);
+  }
+}
