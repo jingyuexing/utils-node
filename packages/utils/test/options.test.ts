69d1f9591fd0f376334469cd1ff40f85f0c2cd0d
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-08-14 14:57:08 +0800

    ✅ test: add testing case

--- packages/utils/test/options.test.ts
+++ packages/utils/test/options.test.ts
@@ -1,4 +1,4 @@
-import { Option } from '@/options';
+import { Option, toOption } from '@/options';
 import { describe, expect, it, Nullable } from 'vitest';
 
 describe('options testing case',()=>{
@@ -16,5 +16,15 @@ describe('options testing case',()=>{
       expect(Option(data).unwrapOr(44)).eq(44)
       data = undefined
       expect(Option(data).unwrap()).eq('None')
+   })
+   it('object Option case', ()=>{
+      let people = {
+         name:"Bob",
+         age:null,
+         address:"Mars"
+      }
+      const optionsPeople = toOption(people)
+      expect(optionsPeople.unwrap().name.unwrapOr("hello")).toEqual('Bob')
+      expect(optionsPeople.unwrap().age.unwrapOr(23)).eq(23)
    })
 })
