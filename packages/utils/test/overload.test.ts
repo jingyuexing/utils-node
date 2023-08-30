4de3c8db3388ac6f53bd1dc127e39cd4ff50dc8b
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-08-31 03:24:32 +0800

    ✅ test: overload testing case

---
+++ packages/utils/test/overload.test.ts
@@ -0,0 +1,27 @@
+import { Overload } from '@/overload';
+import { describe, expect, it, test } from 'vitest';
+
+describe("function overload testing case",()=>{
+   it("two params case",()=>{
+      const addTwo = Overload();
+      addTwo.addImpl("string","string",(a:string,b:string)=>{
+         return a+b;
+      })
+      addTwo.addImpl("number","number",(a:number,b:number)=>{
+         return a+b
+      })
+      expect(addTwo(12,33)).eq(45)
+      expect(addTwo("hello ","world")).eq("hello world")
+   })
+   it("three params case",()=>{
+      const test2 = Overload()
+      test2.addImpl("number","number","number",(a:number,b:number,c:number)=>{
+         return a+b+c;
+      })
+      test2.addImpl("number","string","number",(a:number,b:string,c:number)=>{
+         return `${a + c} ${b}`
+      })
+      expect(test2(1,2,3)).eq(6)
+      expect(test2(1,"A",6)).eq("7 A")
+   })
+})
