9c49ddcb7d5913aa21fd8dc6e6cc0033cd42b89a
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2024-01-03 11:13:11 +0800

    ✨ feat: add `createNestedObject` ✨ feat: add `nestedObject` ✨ feat: add `OmitObjectKeys` ✨ feat: add `PickObjectKeys` ✅ test: add function test case

--- packages/utils/src/object/index.ts
+++ packages/utils/src/object/index.ts
@@ -1,5 +1,6 @@
-import { isArray, isEmpty, isMap, isObject } from '../typeis';
-import type { Entries, Keyof } from '../types'
+import { isArray, isEmpty, isMap, isNone, isObject, isPrimitive, isWeakMap, typeis } from "../typeis";
+import type { Entries, Keyof } from "../types";
+import { getRandomInt } from "@/math";
 /**
  * exclude specified fields
  * @type {T}
@@ -7,32 +8,40 @@ import type { Entries, Keyof } from '../types'
  * @param {(keyof T)[]} excludeName the exclude fields
  */
 export function exclude<T extends object>(obj: T, excludeName: (keyof T)[]) {
-   const keys = Object.keys(obj) as unknown as (keyof T)[];
-   const result: any = {};
-   keys.forEach(key => {
-      if (!excludeName.includes(key)) {
-         result[key] = obj[key];
-      }
-   });
-   return result;
+  const keys = Object.keys(obj) as unknown as (keyof T)[];
+  const result: any = {};
+  keys.forEach((key) => {
+    if (!excludeName.includes(key)) {
+      result[key] = obj[key];
+    }
+  });
+  return result;
 }
+
+type Rename<T extends object, Key extends Record<keyof T, string | number | symbol>> = {
+  [K in keyof T as K extends keyof Key ? Key[K] : K]: K extends keyof T ? T[K] : never;
+};
+
 /**
  * rename object field
  * @param {T} obj the target object
  * @param {{ [P in keyof T]: string }} config change config
  */
-export function renameKey<T extends object & {}>(obj: T, config: { [P in keyof T]: string }) {
-   const result = {};
-   const configKeys = Object.keys(config);
-   configKeys.forEach(key => {
-      (result as any)[(config as any)[key]] = (obj as any)[key];
-   });
-   const others = exclude(obj, configKeys as unknown as (keyof T)[]);
+export function renameKey<T extends object & {}, Key extends Record<keyof T, string | number | symbol>>(
+  obj: T,
+  config: Key,
+): Rename<T, Key> {
+  const result = {};
+  const configKeys = Object.keys(config);
+  configKeys.forEach((key) => {
+    (result as any)[(config as any)[key]] = (obj as any)[key];
+  });
+  const others = exclude(obj, configKeys as unknown as (keyof T)[]);
 
-   return {
-      ...result,
-      ...others,
-   };
+  return {
+    ...result,
+    ...others,
+  };
 }
 /**
  * Convert object to entries form
@@ -45,68 +54,187 @@ export function renameKey<T extends object & {}>(obj: T, config: { [P in keyof T
  * @return {Entries<T>}
  */
 export function entries<T extends any[] | Map<any, any> | (Object & {})>(obj: T): Entries<T> {
-   let entries_: [keyof T, any][] = [];
-   if (isArray(obj)) {
-      obj.forEach((val, index) => {
-         entries_.push([index as Keyof<T>, val]);
-      });
-   } else if (isMap(obj)) {
-      entries_ = [...obj.entries()] as [any, any][];
-   } else if (isObject(obj)) {
-      Object.keys(obj).forEach(key => {
-         entries_.push([key as Keyof<T>, (obj as any)[key]]);
-      });
-   }
-   return entries_ as Entries<T>;
+  let entries_: [keyof T, any][] = [];
+  if (isArray(obj)) {
+    obj.forEach((val, index) => {
+      entries_.push([index as Keyof<T>, val]);
+    });
+  } else if (isMap(obj)) {
+    entries_ = [...obj.entries()] as [any, any][];
+  } else if (isObject(obj)) {
+    Object.keys(obj).forEach((key) => {
+      entries_.push([key as Keyof<T>, (obj as any)[key]]);
+    });
+  }
+  return entries_ as Entries<T>;
 }
 
 export function Extends(target: any, obj: any) {
-   return Object.assign(target, Object.create(obj));
+  return Object.assign(target, Object.create(obj));
 }
 
 /**
  * @param {T} obj the target object
  * @param {K} path the key
+ * @param {K} delimiter the delimiter, default "."
  * @type {T}
  */
-export function objectPath<T extends { [key: string | number]: any }, K extends string | keyof T>(obj: T, path: K): any | undefined {
-   return (path as string).split('.').reduce((value, key) => {
-      if (value && Object.prototype.hasOwnProperty.call(value, key)) {
-         return value[key];
-      }
-      return undefined;
-   }, obj);
+export function objectPath<T extends { [key: string | number]: any }, K extends string | keyof T>(
+  obj: T,
+  path: K,
+  delimiter = ".",
+): any | undefined {
+  return (path as string).split(delimiter).reduce((value, key) => {
+    if (value && Object.prototype.hasOwnProperty.call(value, key)) {
+      return value[key];
+    }
+    return undefined;
+  }, obj);
+}
+
+type NestedObjectValue = string | Record<string, NestedObjectValue>;
+
+type NestedPath<T extends string> = T extends `${infer First}.${infer Rest}`
+  ? First extends string
+    ? { [K in First]: NestedPath<Rest> }
+    : never
+  : T extends string
+  ? { [K in T]: NestedObjectValue }
+  : never;
+
+export function createNestedObject<T extends string>(str: T, value: string, delimiter = "."): NestedPath<T> {
+  const keys = str.split(delimiter) as (keyof NestedPath<T>)[];
+
+  const lastIndex = keys.length - 1;
+  const nestedObject = {} as NestedPath<T>;
+
+  keys.reduce((obj, key, index) => {
+    if (index === lastIndex) {
+      (obj as any)[key] = value;
+    } else {
+      (obj as any)[key] = obj[key] || {};
+    }
+    return obj[key] as NestedPath<T>;
+  }, nestedObject);
+
+  return nestedObject;
+}
+
+export function nestedObject(target: object, path: string, cb: (target: any, key: string) => void): void {
+  const keys: string[] = path.split(".");
+  let currentTarget: any = target;
+
+  for (const key of keys) {
+    if (isObject(currentTarget) && currentTarget.hasOwnProperty(key)) {
+      currentTarget = currentTarget[key];
+      cb(currentTarget, key);
+    } else {
+      throw new Error("Unreachable access path");
+    }
+  }
 }
 
 /**
-  * Recursively filter out null values in the object
-  * @param {Object|Array} obj the object to be filtered
-  * @returns {Object|Array} Object after filtering out empty values
-  */
+ * Recursively filter out null values in the object
+ * @param {Object|Array} obj the object to be filtered
+ * @returns {Object|Array} Object after filtering out empty values
+ */
 export function filterEmpty<T extends any[] | Map<any, any> | (Object & {})>(obj: T): any {
-   return Array.isArray(obj)
-      ? obj.reduce((acc, cur) => {
-         if (isEmpty(cur)) { return acc }
-         if (isObject(cur) || isArray(cur)) {
-            const filtered = filterEmpty(cur);
-            if (!isEmpty(filtered)) {
-               return [...acc, filtered];
-            }
-         } else {
-            return [...acc, cur];
-         }
+  return Array.isArray(obj)
+    ? obj.reduce((acc, cur) => {
+        if (isEmpty(cur)) {
+          return acc;
+        }
+        if (isObject(cur) || isArray(cur)) {
+          const filtered = filterEmpty(cur);
+          if (!isEmpty(filtered)) {
+            return [...acc, filtered];
+          }
+        } else {
+          return [...acc, cur];
+        }
       }, [] as any[])
-      : Object.entries(obj as { [key: string]: any }).reduce((acc, [key, value]) => {
-         if (isEmpty(value)) {
-            return acc;
-         }
-         if (isObject(value) || isArray(value)) {
-            const filtered = filterEmpty(value);
-            if (!isEmpty(filtered)) {
-               return { ...acc, [key]: filtered };
-            }
-         } else {
-            return { ...acc, [key]: value }
-         }
+    : Object.entries(obj as { [key: string]: any }).reduce((acc, [key, value]) => {
+        if (isEmpty(value)) {
+          return acc;
+        }
+        if (isObject(value) || isArray(value)) {
+          const filtered = filterEmpty(value);
+          if (!isEmpty(filtered)) {
+            return { ...acc, [key]: filtered };
+          }
+        } else {
+          return { ...acc, [key]: value };
+        }
       }, {} as any);
+}
+/**
+ * Creates a new object by omitting specified keys from the input object.
+ * @param {T} object - The input object from which keys will be omitted.
+ * @param {K[]} keys - An array of keys to be omitted from the object.
+ * @returns {Omit<T, K>}  A new object without the specified keys.
+ */
+export function OmitObjectKeys<T extends object & {}, K extends keyof T>(object: T, keys: K[]): Omit<T, K> {
+  const OmitObject = {} as Omit<T, K>;
+  Object.keys(object).forEach((key) => {
+    if (keys.indexOf(key as K) === -1) {
+      (OmitObject as any)[key] = (object as any)[key];
+    }
+  });
+  return OmitObject;
+}
+
+/**
+ * Creates a new object by picking specified keys from the input object.
+ * @param {T} object - The input object from which keys will be picked.
+ * @param {K[]} keys - An array of keys to be picked from the object.
+ * @returns {Pick<T, K>}  A new object containing only the specified keys.
+ */
+export function PickObjectKeys<T extends object, K extends keyof T>(object: T, keys: K[]): Pick<T, K> {
+  const pickedObject = {} as Pick<T, K>;
+  Object.keys(object).forEach((key) => {
+    if (keys.indexOf(key as K) !== -1) {
+      (pickedObject as any)[key] = (object as any)[key];
+    }
+  });
+  return pickedObject;
+}
+
+export function randomChoosen<T>(arr: T[], nums: number) {
+  const result: T[] = [];
+  for (let i = 0; i < nums; i++) {
+    result.push(arr[getRandomInt(0, arr.length)]);
+  }
+  return result;
+}
+
+export function traverseObject(target: object, sym = "."): Record<string, any> {
+  const record: Record<string, any> = {};
+
+  const traverse = (obj: any, path: string[]) => {
+    if (typeof obj === "object") {
+      for (const key in obj) {
+        switch (typeof obj[key]) {
+          case "object":
+            if (isArray(obj[key]) || isMap(obj[key]) || isWeakMap(obj[key])) {
+              (obj[key] as any[]).forEach((value, idx) => {
+                traverse(value, path.concat(`${key}${sym}${idx}`));
+              });
+            } else {
+              traverse(obj[key], path.concat(key));
+            }
+            break;
+          case "function":
+            break;
+          default:
+            record[path.concat(key).join(sym)] = obj[key];
+        }
+      }
+    } else {
+      record[path.join(sym)] = obj;
+    }
+  };
+  traverse(target, []);
+
+  return record;
 }
--- packages/utils/test/object.test.ts
+++ packages/utils/test/object.test.ts
@@ -1,11 +1,11 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from "vitest";
 
-import { entries, Extends, exclude, renameKey } from '@/object';
+import { entries, Extends, exclude, renameKey, traverseObject, OmitObjectKeys, PickObjectKeys } from "@/object";
 
-describe('object testing', () => {
-   it('test entries', () => {
-      let obj: any[] = [12, 45, 'hello'];
-      expect(entries(obj)).toMatchInlineSnapshot(`
+describe("object testing", () => {
+  it("test entries", () => {
+    let obj: any[] = [12, 45, "hello"];
+    expect(entries(obj)).toMatchInlineSnapshot(`
         [
           [
             0,
@@ -21,12 +21,12 @@ describe('object testing', () => {
           ],
         ]
       `);
-      let map = new Map<string, any>();
-      map.set('a', 1);
-      map.set('b', 34);
-      map.set('name', 36);
+    let map = new Map<string, any>();
+    map.set("a", 1);
+    map.set("b", 34);
+    map.set("name", 36);
 
-      expect(entries(map)).toMatchInlineSnapshot(`
+    expect(entries(map)).toMatchInlineSnapshot(`
         [
           [
             "a",
@@ -42,7 +42,7 @@ describe('object testing', () => {
           ],
         ]
       `);
-      expect(entries([12, 33, 56, 89])).toMatchInlineSnapshot(`
+    expect(entries([12, 33, 56, 89])).toMatchInlineSnapshot(`
         [
           [
             0,
@@ -62,12 +62,12 @@ describe('object testing', () => {
           ],
         ]
       `);
-      expect(
-         entries({
-            name: 'Typescript',
-            type: 'Programing Language',
-         }),
-      ).toMatchInlineSnapshot(`
+    expect(
+      entries({
+        name: "Typescript",
+        type: "Programing Language",
+      }),
+    ).toMatchInlineSnapshot(`
         [
           [
             "name",
@@ -79,39 +79,108 @@ describe('object testing', () => {
           ],
         ]
       `);
-   });
-   it('test exclude', () => {
-      expect(
-         exclude(
-            {
-               name: 'balabala',
-               age: 33,
-            },
-            ['age'],
-         ),
-      ).toMatchInlineSnapshot(`
+  });
+  it("test exclude", () => {
+    expect(
+      exclude(
+        {
+          name: "balabala",
+          age: 33,
+        },
+        ["age"],
+      ),
+    ).toMatchInlineSnapshot(`
         {
           "name": "balabala",
         }
       `);
-   });
-   it('testing rename key', () => {
-      expect(
-         renameKey(
-            {
-               a: 'Bib',
-               b: '1h',
-            },
-            {
-               a: 'name',
-               b: 'time',
-            },
-         ),
-      ).toMatchInlineSnapshot(`
+  });
+  it("testing rename key", () => {
+    expect(
+      renameKey(
+        {
+          a: "Bib",
+          b: "1h",
+        },
+        {
+          a: "name",
+          b: "time",
+        },
+      ),
+    ).toMatchInlineSnapshot(`
         {
           "name": "Bib",
           "time": "1h",
         }
       `);
-   });
+  });
+  it("test traverseObject",()=>{
+   const obj = {
+      a:{
+         b:{
+            kol:{
+               final:12
+            }
+         },
+         x:{
+            m:[true,false,"some"]
+         }
+      }
+   }
+   const traverse = traverseObject(obj)
+   console.table(traverse)
+   expect(traverse['a.b.kol.final']).eq(12)
+   expect(traverse['a.x.m.0']).eq(true)
+   expect(traverse['a.x.m.1']).eq(false)
+   expect(traverse['a.x.m.2']).eq("some")
+
+
+   const traverse2 = traverseObject(obj,"\\")
+   console.table(traverse2)
+   expect(traverse2['a\\b\\kol\\final']).eq(12)
+   expect(traverse2['a\\x\\m\\0']).eq(true)
+   expect(traverse2['a\\x\\m\\1']).eq(false)
+   expect(traverse2['a\\x\\m\\2']).eq("some")
+
+   const traverse3 = traverseObject(obj,"/")
+   console.table(traverse3)
+   expect(traverse3['a/b/kol/final']).eq(12)
+   expect(traverse3['a/x/m/0']).eq(true)
+   expect(traverse3['a/x/m/1']).eq(false)
+   expect(traverse3['a/x/m/2']).eq("some")
+
+   const traverse4 = traverseObject(obj,"->")
+   console.table(traverse4)
+   expect(traverse4['a->b->kol->final']).eq(12)
+   expect(traverse4['a->x->m->0']).eq(true)
+   expect(traverse4['a->x->m->1']).eq(false)
+   expect(traverse4['a->x->m->2']).eq("some")
+
+   const traverse5 = traverseObject(obj,")")
+   console.table(traverse5)
+   expect(traverse5['a)b)kol)final']).eq(12)
+   expect(traverse5['a)x)m)0']).eq(true)
+   expect(traverse5['a)x)m)1']).eq(false)
+   expect(traverse5['a)x)m)2']).eq("some")
+  })
+
+  it("test Omit object",()=>{
+   const omitObj = OmitObjectKeys({
+      name:"",
+      gender:"",
+      AOL:12
+   },["name"])
+   console.table(omitObj)
+   expect(Object.keys(omitObj).includes('name')).eq(false)
+   expect(Object.keys(omitObj).includes('gender')).eq(true)
+  })
+  it("test Pick Object",()=>{
+   const pickObj = PickObjectKeys({
+      name:"No party of Cao dong",
+      other:""
+   },["name"])
+   expect(Object.keys(pickObj).includes("name")).eq(true)
+   expect(Object.keys(pickObj).includes("other")).eq(false)
+  })
 });
+
