7d509b97828c0760db20c7c407d06ce899c6f4f9
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2024-02-01 08:43:53 +0800

    🚮 delete: remove `chineseNumber`

--- packages/utils/test/chineseNumber.test.ts
+++
@@ -1,9 +0,0 @@
-import { arabicToChinese } from "@/chineseNumber"
-import { describe, it, expect } from "vitest"
-
-describe("chinese number system",()=>{
-   it("test number to chinese",()=>{
-      let s = arabicToChinese(1032)
-      console.log(s)
-   })
-})
