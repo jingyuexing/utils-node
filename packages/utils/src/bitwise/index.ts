93c667c3d166725c8e27b6c56fdadcb2e4a0cf29
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2024-06-09 19:55:14 +0800

    ✨ feat: improve bitwise

--- packages/utils/src/bitwise/index.ts
+++ packages/utils/src/bitwise/index.ts
@@ -1,23 +1,10 @@
 import { countBits } from "@/math"
 
-export function fill(target:number[],val:number):number[] {
-   var mask:number,fullmask:number
-   mask = 0xff
-   let bit = countBits(val)
-   fullmask = ((1 << bit) - 1)
-   mask = ( fullmask >> 8) ^ mask
-   while(mask != 0){
-      let c = val & mask
-      val = val & (mask ^ fullmask)
-      console.table({
-         hex:c.toString(16),
-         cache:c,
-         value:val
-      })
-      target.push(c)
-      mask = mask >> 8
-      fullmask = fullmask >> 8
-      bit = bit - 8
+export function fill(target: number[], val: number): number[] {
+   let _value = val;
+   while (_value !== 0) {
+      target.push(_value & 0xff)
+      _value = _value >>> 8
    }
-   return target
+   return target.reverse()
 }
