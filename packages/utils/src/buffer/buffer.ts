b3a3782d15988851b45d912bd06592ebf9d23d99
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2024-06-09 19:48:26 +0800

    ✨ feat: string to buffer

--- packages/utils/src/buffer/buffer.ts
+++ packages/utils/src/buffer/buffer.ts
@@ -1,3 +1,5 @@
+import { fill } from "@/bitwise"
+
 export class ABuffer<T extends number>{
    val:T[]
    constructor(length?:number){
@@ -21,4 +23,44 @@ export class ABuffer<T extends number>{
    includes(ele:T,index?:number){
       return this.val.includes(ele,index)
    }
+}
+
+export function textoBuffer(text:string){
+   if(typeof TextEncoder !== "undefined"){
+      return new TextEncoder().encode(text)
+   }
+   let temp = []
+   for(let i =0;i<text.length;i++){
+      temp.push(...fill([],text.charCodeAt(i)))
+   }
+   const buffer = Uint8Array.from(temp)
+   return buffer.buffer
+}
+
+export function bufferToText(buffer:ArrayBuffer,option?:TextDecoderOptions){
+   if(typeof TextDecoder !== "undefined"){
+      return new TextDecoder(undefined,option).decode(buffer)
+   }
+   return ""
+}
+
+export function fileToBuffer(chunks:Blob):Promise<ArrayBuffer>{
+ return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", (ev) => {
+      const result = ev.target?.result;
+      if (result) {
+        resolve(Buffer.from(result as ArrayBuffer));
+      } else {
+        reject(new Error("Failed to read file"));
+      }
+    });
+
+    reader.addEventListener("error", (err) => {
+      reject(err);
+    });
+
+    reader.readAsArrayBuffer(chunks);
+  });
 }
