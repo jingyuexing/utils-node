4fa42322d4f70f208e3e6c482f2286d51abfad48
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2024-06-09 19:47:44 +0800

    ✨ feat:support cidr

--- packages/utils/src/IPv4/index.ts
+++ packages/utils/src/IPv4/index.ts
@@ -2,11 +2,17 @@ import { isNumber, isString } from '../typeis';
 
 export class IPv4 {
    ipv4: string = '';
+   range?: [string, string]
    integer: number = 0;
 
    constructor(ip: string | number) {
+
       if (isString(ip)) {
-         this.ipv4 = ip;
+         if (ip.indexOf("/") !== -1) {
+            this.range = this._cidr(ip)
+         } else {
+            this.ipv4 = ip;
+         }
       } else if (isNumber(ip)) {
          this.integer = ip;
       }
@@ -16,7 +22,7 @@ export class IPv4 {
     * @param {number} ip [description]
     */
    _toString(ip: number) {
-      return `${(ip >> 24) & 0xff}.${(ip >> 16) & 0xff}.${(ip >> 8) & 0xff}.${(ip >> 0) & 0xff}`;
+      return `${(ip >> 24) & 0xff}.${(ip >> 16) & 0xff}.${(ip >> 8) & 0xff}.${ip & 0xff}`;
    }
    /**
     * [_toNumber description]
@@ -26,13 +32,20 @@ export class IPv4 {
       if (address !== '') {
          let ipNumber = 0;
          const partIp = address.split('.');
-         let i = 0;
          for (const ipart of partIp) {
-            ipNumber += parseInt(ipart) * 0x100 ** (3 - i);
-            i++;
+            ipNumber = (ipNumber << 8) | parseInt(ipart,10);
          }
          return ipNumber;
       }
+   }
+   _cidr(ip: string) {
+      const [ip_, prefix] = ip.split('/');
+      const ipnumber = this._toNumber(ip_)!
+      const subnetMask = (0xFFFFFFFF << (32 - Number(prefix))) >>> 0;
+
+      const networkAddress = ipnumber & subnetMask;
+      const broadcastAddress = networkAddress | (~subnetMask >>> 0);
+      return [this._toString(networkAddress + 1), this._toString(broadcastAddress - 1)] as [string, string]
    }
    toString() {
       return this._toString(this.integer);
