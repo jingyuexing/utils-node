bc319d935afafe1e0672418ef22b5421197db692
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2024-06-16 11:20:32 +0800

    ✨ feat: improve reactive, add filter ✅ test: add testing case

--- packages/utils/src/reactive/index.ts
+++ packages/utils/src/reactive/index.ts
@@ -1,53 +1,58 @@
 import { isUndefined } from "../typeis";
-
-export function reactive<T extends object & {}>(obj: T) {
-  let _getter: <S>(target: T, key: string | symbol) => S
-  let _setter: <S>(target: T, key: string | symbol, value: any) => S
-  let _reactiveObject: T = {} as T;
-  const handler: ProxyHandler<T> = {
-    get(target, key) {
-      if (!isUndefined(_getter)) {
-        _getter(target, key)
+/**
+ * @type {T}
+ * @param {T} obj the target object
+ * @param {(key:keyof T,value:T[keyof T],target:T)=>boolean} filter the filter
+ */
+export function reactive<T extends object & {}>(obj: T, filter?: (key: keyof T, value: T[keyof T], target?: T) => boolean) {
+   let _getter: <S>(target: T, key: string | symbol) => S
+   let _setter: <S>(target: T, key: string | symbol, value: any) => S
+   let _reactiveObject: T = {} as T;
+   let defaultFilter = filter || function(key,value,target){return true};
+   const handler: ProxyHandler<T> = {
+      get(target, key) {
+         if (!isUndefined(_getter) && defaultFilter(key as keyof T, Reflect.get(target,key), _reactiveObject)) {
+            _getter(target, key)
+         }
+         return Reflect.get(target, key, _reactiveObject);
+      },
+      set(target, key, value) {
+         if (!isUndefined(_setter) && defaultFilter(key as keyof T, value, target)) {
+            _setter(target, key, value)
+         }
+         return Reflect.set(target, key, value, _reactiveObject);
+      },
+   };
+   if (typeof Proxy !== "undefined") {
+      _reactiveObject = new Proxy(obj, handler);
+   } else {
+      for (const key in obj) {
+         Object.defineProperty(obj, key, {
+            get() {
+               if (!isUndefined(_getter) && defaultFilter(key as keyof T, obj[key], _reactiveObject)) {
+                  _getter(obj, key)
+               }
+               return obj[key];
+            },
+            set(value) {
+               if (!isUndefined(_setter) && defaultFilter(key as keyof T, obj[key], _reactiveObject)) {
+                  _setter(obj, key, value)
+               }
+               obj[key] = value;
+            },
+         });
       }
-      return Reflect.get(target, key, _reactiveObject);
-    },
-    set(target, key, value) {
-      if (!isUndefined(_setter)) {
-        _setter(target, key, value)
-      }
-      return Reflect.set(target, key, value, _reactiveObject);
-    },
-  };
-  if (typeof Proxy !== "undefined") {
-    _reactiveObject = new Proxy(obj, handler);
-  } else {
-    for (const key in obj) {
-      Object.defineProperty(obj, key, {
-        get() {
-          if (!isUndefined(_getter)) {
-            _getter(obj, key)
-          }
-          return obj[key];
-        },
-        set(value) {
-          if (!isUndefined(_setter)) {
-            _setter(obj, key, value)
-          }
-          obj[key] = value;
-        },
-      });
-    }
-    _reactiveObject = obj;
-  }
-  const getter = (cb: (<S>(target: T, key: keyof T | string | symbol) => S)) => {
-    _getter = cb
-  }
-  const setter = (cb: <S>(target: T, key: keyof T | string | symbol, value: any) => S) => {
-    _setter = cb
-  }
-  return {
-    values: _reactiveObject,
-    getter,
-    setter
-  }
+      _reactiveObject = obj;
+   }
+   const getter = (cb: (<S>(target: T, key: keyof T | string | symbol) => S)) => {
+      _getter = cb
+   }
+   const setter = (cb: <S>(target: T, key: keyof T | string | symbol, value: T[keyof T]) => S) => {
+      _setter = cb
+   }
+   return {
+      values: _reactiveObject,
+      getter,
+      setter
+   }
 }
--- packages/utils/test/reactive.test.ts
+++ packages/utils/test/reactive.test.ts
@@ -1,14 +1,15 @@
 import { reactive } from "@/reactive";
 import { describe, expect, it } from "vitest";
+import { isNumber } from "..";
 
-describe("testing reactive",()=>{
-   it("testing reactive: setter",()=>{
-      const { values, setter,getter } = reactive({
-         name:"A9",
-         age:12
+describe("testing reactive", () => {
+   it("testing reactive: setter", () => {
+      const { values, setter, getter } = reactive({
+         name: "A9",
+         age: 12
       })
-      setter((tartget,key,value)=>{
-         if(value === "A100"){
+      setter((tartget, key, value) => {
+         if (value === "A100") {
             console.log(tartget)
          }
       })
@@ -16,5 +17,24 @@ describe("testing reactive",()=>{
       values.age = 20
       expect(values.name).eq("A100")
       expect(values.age).eq(20)
+   })
+   it("testing reactive filter", () => {
+      const { values, setter, getter } = reactive({
+         a: "12",
+         b: 33,
+         c: 44,
+         f: "12"
+      }, (k, v) => !isNumber(v))
+      let s = 0
+      setter((t, k, v) => {
+         s++
+         console.log(k)
+      })
+      values.a = "233333"
+      getter((t, key) => {
+         console.log(key)
+      })
+      expect(s).eq(1)
+
    })
 })
